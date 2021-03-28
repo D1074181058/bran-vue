74310ec21940ce5cf2100e3a1e36ed3665e78400
D1074181058 <D1074181058@gm.lhu.edu.tw>
2021-03-29 02:22:04 +0800

    0329

--- myapp/models.py
+++ myapp/models.py
@@ -10,7 +10,7 @@ class member(models.Model):
     email = models.EmailField(max_length=100, null=False)
     date = models.CharField(max_length=20, null=True,blank=True,default='')
     phone = models.CharField(max_length=10, null=True,blank=True,default='')
-    address = models.CharField(max_length=47, null=True,blank=True,default='')
+    address = models.CharField(max_length=49, null=True,blank=True,default='')
 
     def __str__(self):
        return self.Name
