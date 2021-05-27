274dd42215c8010129e03e716dbf8f3e8d802231
D1074181058 <D1074181058@gm.lhu.edu.tw>
2021-05-27 11:38:13 +0800

    no message

--- myapp/form.py
+++ myapp/form.py
@@ -3,10 +3,10 @@ from captcha.fields import CaptchaField
 
 
 class signupform(forms.Form):
-    Name = forms.CharField(min_length=2,max_length=10, initial='',widget=forms.TextInput(attrs={'placeholder': '吳宗翰','autocomplete':'off'}))
-    account = forms.CharField(max_length=10,initial='',widget=forms.TextInput(attrs={'autocomplete':'off'}))
-    password = forms.CharField(max_length=20,initial='',widget=forms.PasswordInput(attrs={'autocomplete':'off'}))
-    email = forms.EmailField(max_length=100,initial='',widget=forms.TextInput(attrs={'autocomplete':'off',}))
+    Name = forms.CharField(min_length=2,max_length=10, initial='',widget=forms.TextInput(attrs={'placeholder': '王小明(必填)','autocomplete':'off'}))
+    account = forms.CharField(max_length=10,initial='',widget=forms.TextInput(attrs={'placeholder': '(必填)','autocomplete':'off'}))
+    password = forms.CharField(max_length=20,initial='',widget=forms.PasswordInput(attrs={'placeholder': '(必填)','autocomplete':'off'}))
+    email = forms.EmailField(max_length=100,initial='',widget=forms.TextInput(attrs={'placeholder': '(必填)','autocomplete':'off',}))
     date = forms.DateField(required=False,widget=forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD','autocomplete':'off'}))
     phone = forms.CharField(max_length=10, required=False,initial='',widget=forms.TextInput(attrs={'autocomplete':'off'}))
     address = forms.CharField(max_length=47, required=False,initial='',widget=forms.TextInput(attrs={'autocomplete':'off'}))
